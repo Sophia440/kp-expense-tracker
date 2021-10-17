a73ab0abe93cc4b2ac2d6b27cd56eb459a8d448d
Sophia Klimovich <klimovi4.sophia@gmail.com>
2021-10-17 18:07:52 +0300

    rewrote first week's code into components

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,5 +1,8 @@
+import React from 'react'
+
 const balance = document.getElementById('balance');
 
+// TODO find out how to call functions correctly
 function increase() {
     let current = 0.0
     current += parseFloat(balance.innerText.substring(1));
@@ -12,4 +15,15 @@ function decrease() {
     current += parseFloat(balance.innerText.substring(1));
     current -= 1.0
     balance.innerText = "$" + current.toString() + ".00";
+}
+
+export const Balance = () => {
+    return (
+        <div>
+            <h2>Your Balance</h2>
+            <h3 id="balance">$0.00</h3>
+            <button type="button" className="btn" onClick="increase()">+</button>
+            <button type="button" className="btn" onClick="decrease()">–</button>
+        </div>
+    )
 }
