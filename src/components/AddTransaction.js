f325f735e4430260c506c9eea062eb1818e51958
Sophia Klimovich <klimovi4.sophia@gmail.com>
2021-11-13 12:23:52 +0300

    tried out using localStorage in AddTransaction component

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,22 +1,56 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import {v4} from 'uuid'
+import {randomColor} from 'randomcolor'
 
 export const AddTransaction = () => {
+    const [text, setText] = useState('');
+    const [amount, setAmount] = useState(0);
+
+    const [transactions, setTransactions] = useState(
+        JSON.parse(localStorage.getItem('transactions')) || []
+    );
+
+    useEffect(() => {
+        localStorage.setItem('transactions', JSON.stringify(transactions))
+    }, [transactions]);
+
+    const newTransaction = () => {
+        if (text.trim() !== '') {
+            const newTransaction = {
+                id: v4(),
+                text: text,
+                amount: amount,
+                color: randomColor({
+                    luminosity: 'light'
+                })
+
+            };
+            setTransactions((transactions) => [...transactions, newTransaction])
+        } else {
+            alert('Input something');
+        }
+        setText('')
+        setAmount(0)
+    }
+
     return (
         <>
             <h4>Add new transaction</h4>
             <form>
                 <div className="form-control">
                     <label htmlFor="text">Text</label>
-                    <input type="text" className="form-field" placeholder="Enter text..."/>
+                    <input type="text" value={text} onChange={(e) => setText(e.target.value)}
+                           placeholder="Enter text..."/>
                 </div>
                 <div className="form-control">
                     <label htmlFor="amount"
                     >Amount <br/>
                         (negative - expense, positive - income)</label
                     >
-                    <input type="number" placeholder="Enter amount..."/>
+                    <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)}
+                           placeholder="Enter amount..."/>
                 </div>
-                <button className="btn">Add transaction</button>
+                <button className="btn" onClick={newTransaction}>Add transaction</button>
             </form>
         </>
     )
